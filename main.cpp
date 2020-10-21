07112913a78227570d025531bb2571791766bdc5
SergiyStepanchuk <serhii.stepanchuk.itis.2020@lpnu.ua>
2020-10-21 14:08:01 +0300

    init

---
+++ main.cpp
@@ -0,0 +1,25 @@
+// main.cpp 
+// Степанчук Сергій 
+// Лабораторна робота № 3.4 
+// Розгалуження, задане плоскою фігурою
+// Варіант 20
+
+#include <iostream> 
+#include <cmath>
+
+using namespace std;
+
+void main() {
+    double x, y; // input
+    double m; // additional
+
+    cout << "x = "; cin >> x;
+    cout << "y = "; cin >> y;
+
+    if ((y <= -x && y >= pow(x - 2, 2) - 3) ||
+        (y >= 0 && y <= pow(x - 2, 2) - 3 && y <= x))
+        cout << "yes";
+    else cout << "no";
+
+    cout << endl;
+}
