3259684ee631c366e087e4f1447e59136a791bc1
SergiyStepanchuk <serhii.stepanchuk.itis.2020@lpnu.ua>
2020-10-25 19:01:38 +0200

    bbsda

--- main.cpp
+++ main.cpp
@@ -11,13 +11,17 @@ using namespace std;
 
 void main() {
     double x, y; // input
-    double m; // additional
 
     cout << "x = "; cin >> x;
     cout << "y = "; cin >> y;
 
-    if ((y <= -x && y >= pow(x - 2, 2) - 3) ||
-        (y >= 0 && y <= pow(x - 2, 2) - 3 && y <= x))
+    if ((
+        y <= -x && 
+        y >= pow(x - 2, 2) - 3) 
+        ||
+        (y >= 0 && 
+         y >= pow(x - 2, 2) - 3 && 
+         y <= x))
         cout << "yes";
     else cout << "no";
 
